40ce5f12a0010a2cbd4403739f7427214c5af86c
klchnkvv <daniel.kliuchnikov@mail.ru>
2017-12-05 01:47:47 +0300

    Implemented 10 test cases

---
+++ icelandair-automation-testing/icelandair_spec.rb
@@ -0,0 +1,157 @@
+require_relative 'spec/spec_helper'
+
+describe 'icelandair-selenium-testing' do
+  before :all do
+    initalize_selenium
+  end
+
+  after :all do
+    sleep 10
+    @driver.quit
+  end
+
+  it 'test case #1', :test_case_1 => true do
+    @departure.clear
+    @departure.send_keys "Dallas/Ft Worth (DFW) USA"
+    just_click_anywhere
+    @arrive.clear
+    @arrive.send_keys "Helsinki (HEL) Finland"
+    just_click_anywhere
+    establish_date
+    passengers_count 2, 0, 0
+    @driver.execute_script("arguments[0].innerText = '#{COMFORT_CLASSES[0]}'", @select_comfort)
+    @search.submit
+  end
+
+  it 'test case  #2', :test_case_2 => true do
+    dates_clear
+    @departure.clear
+    @departure.send_keys 'Reykjavik'
+    just_click_anywhere
+    @arrive.clear
+    @arrive.send_keys 'Helsinki'
+    just_click_anywhere
+    establish_date
+    passengers_count 2, 0, 1
+    @driver.execute_script("arguments[0].innerText = '#{COMFORT_CLASSES[1]}'", @select_comfort)
+    @search.submit
+  end
+
+  it 'test case #3', :test_case_3 => true do
+    dates_clear
+    @one_way.click
+    @departure.clear
+    @departure.send_keys 'New York'
+    just_click_anywhere
+    @arrive.clear
+    @arrive.send_keys 'Helsinki'
+    just_click_anywhere
+    @depart_date.send_keys DEPART_DATE
+    passengers_count 2, 0, 1
+    @driver.execute_script("arguments[0].innerText = '#{COMFORT_CLASSES[2]}'", @select_comfort)
+    @search.submit
+  end
+
+  it 'test case #4', :test_case_4 => true do
+    @depart_date.clear
+    @round_trip.click
+    @departure.clear
+    @departure.send_keys 'Anchorage'
+    just_click_anywhere
+    @arrive.clear
+    @arrive.send_keys 'London'
+    just_click_anywhere
+    establish_date
+    passengers_count 2, 0, 1
+    @driver.execute_script("arguments[0].innerText = '#{COMFORT_CLASSES[1]}'", @select_comfort)
+    @search.submit
+  end
+
+  it 'test case #5', :test_case_5 => true do
+    @one_way.click
+    @depart_date.clear
+    @departure.clear
+    @departure.send_keys 'New York (KENNEDY)'
+    just_click_anywhere
+    @arrive.clear
+    @arrive.send_keys 'Reykjavik (KEF) Iceland'
+    just_click_anywhere
+    @depart_date.send_keys DEPART_DATE
+    passengers_count 1, 1, 0
+    @driver.execute_script("arguments[0].innerText = '#{COMFORT_CLASSES[1]}'", @select_comfort)
+    @search.submit
+  end
+
+  it 'test case #6', :test_case_6 => true do
+    @stopover.click
+    sleep 1
+    @depart_date.clear
+    @departure.clear
+    @departure.send_keys 'Chicago'
+    just_click_anywhere
+    @arrive.clear
+    @arrive.send_keys 'Reykjavik (KEF) Iceland'
+    just_click_anywhere
+    @driver.find_element(:id, 'dep_2').send_keys 'Reykjavik (KEF) Iceland'
+    just_click_anywhere
+    @driver.find_element(:id, 'arr_2').send_keys 'Chicago (CHI) United States'
+    just_click_anywhere
+    @depart_date.send_keys DEPART_DATE
+    @driver.execute_script("return arguments[0].value='#{(Time.now + 14*24*60*60).strftime('%m/%d/%Y')}'", (@driver.find_element :id, 'departDate-2'))
+    passengers_count 3, 0, 0
+    @driver.execute_script("arguments[0].innerText = '#{COMFORT_CLASSES[1]}'", @select_comfort)
+    @search.submit
+  end
+
+  it 'test case #7', :hotels => true do
+    (@driver.find_element :link_text, 'Hotels').click
+    (@driver.find_element :id, 'ss').send_keys 'Tokyo, Tokyo Prefecture, Japan', :tab, :tab
+    (@driver.find_element :name, 'checkin_month').send_keys Time.now.strftime('%m')
+    (@driver.find_element :name, 'checkin_monthday').send_keys (Time.now + 4*24*60*60).strftime('%d')
+    (@driver.find_element :name, 'checkin_year').send_keys (Time.now + 4*24*60*60).strftime('%Y'), :tab
+    (@driver.find_element :id, 'no_rooms').send_keys 2
+    (@driver.find_element :id, 'group_adults').send_keys 3
+    (@driver.find_element :class, 'sb-searchbox').submit
+  end
+
+  it 'test case #8', :test_case_8 => true do
+    @one_way.click
+    @depart_date.clear
+    @departure.clear
+    @departure.send_keys 'Philadelphia'
+    just_click_anywhere
+    @arrive.clear
+    @arrive.send_keys 'Amsterdam'
+    just_click_anywhere
+    @depart_date.send_keys DEPART_DATE
+    passengers_count 2, 0, 0
+    @driver.execute_script("arguments[0].innerText = '#{COMFORT_CLASSES[2]}'", @select_comfort)
+    @search.submit
+  end
+
+  it 'test case #9', :test_case_9 => true do
+    @one_way.click
+    @depart_date.clear
+    @departure.clear
+    @departure.send_keys 'London'
+    just_click_anywhere
+    @arrive.clear
+    @arrive.send_keys 'Boston'
+    just_click_anywhere
+    @depart_date.send_keys DEPART_DATE
+    passengers_count 1, 0, 2
+    @driver.execute_script("arguments[0].innerText = '#{COMFORT_CLASSES[2]}'", @select_comfort)
+    @search.submit
+  end
+
+  it 'test case #10', :cars => true do
+    (@driver.find_element :link_text, 'Cars').click
+    (@driver.find_element :id, 'puSearchInput').send_keys 'Boston - Logan Intl, US'
+    (@driver.find_element :id, 'pu_date').clear
+    (@driver.find_element :id, 'pu_date').send_keys Time.now.strftime('%m/%d/%Y')
+    (@driver.find_element :id, 'do_date').clear
+    (@driver.find_element :id, 'do_date').send_keys (Time.now + 2*24*60*60).strftime('%m/%d/%Y')
+    (@driver.find_element :id, 'formsubmit').submit
+  end
+
+end
---
+++ icelandair-automation-testing/spec/spec_helper.rb
@@ -0,0 +1,75 @@
+require 'selenium-webdriver'
+require 'touch_action'
+
+DEPART_DATE = (Time.now + 4*24*60*60).strftime('%m %d %Y')
+RETURN_DATE = (Time.now + 14*24*60*60).strftime('%m %d %Y')
+COMFORT_CLASSES = ['Economy', 'Economy Comfort', 'Saga Class']
+
+RSpec.configure do |config|
+
+  config.expect_with :rspec do |expectations|
+
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+
+  def press_depart_enter
+    @driver.action.send_keys @departure, :return
+  end
+
+  def press_arrive_enter
+    @driver.action.send_keys @departure, :return
+  end
+
+  def initalize_selenium
+    @driver = Selenium::WebDriver.for :chrome
+    @driver.navigate.to 'http://www.icelandair.com/'
+    @driver.find_element(:class, 'us').click
+
+    @one_way = @driver.find_element :id, 'oneway'
+    @round_trip = @driver.find_element(:id, 'return')
+    @stopover = @driver.find_element(:id, 'stopover')
+    @departure = @driver.find_element(:id, 'dep_1')
+    @arrive = @driver.find_element(:id, 'arr_1')
+    @depart_date = @driver.find_element(:id, 'departDate-1')
+    @arrival_date = @driver.find_element(:name, 'arrivalDate')
+    @select_comfort = @driver.find_element :xpath, "//span[@class='center']"
+    @search = @driver.find_element :xpath, "//input[@id='search_now']"
+    @adults = @driver.find_element :id, 'ADT'
+    @children = @driver.find_element :id, 'CHD'
+    @infants = @driver.find_element :id, 'INF'
+  end
+
+  def dates_clear
+    @depart_date.clear
+    @arrival_date.clear
+  end
+
+  def passengers_count adults, children, infants
+    pax_selects = @driver.find_element :class, 'pax_selects'
+    @driver.execute_script("arguments[0].setAttribute('style', 'display: block')", pax_selects)
+    @adults.clear
+    @adults.send_keys adults
+    @children.clear
+    @children.send_keys children
+    @infants.clear
+    @infants.send_keys infants
+  end
+
+  def establish_date
+    @depart_date.send_keys DEPART_DATE
+    @arrival_date.send_keys RETURN_DATE
+  end
+
+  def just_click_anywhere
+    sleep 2
+    (@driver.find_element :id, 'header').click
+  end
+
+end
